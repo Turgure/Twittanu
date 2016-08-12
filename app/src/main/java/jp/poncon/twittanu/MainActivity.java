e522d87a2c4f2ed065459b5b768da78c6780d926
Turgure <toranana_bye@yahoo.co.jp>
2016-08-12 18:38:39 +0900

    create MainActivity

---
+++ app/src/main/java/jp/poncon/twittanu/MainActivity.java
@@ -0,0 +1,16 @@
+package jp.poncon.twittanu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Shigure on 2016/08/12.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
