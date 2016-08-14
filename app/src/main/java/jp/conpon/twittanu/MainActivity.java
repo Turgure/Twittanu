eca741d749529863c7ba2e3656ebd35487c10dd5
Turgure <toranana_bye@yahoo.co.jp>
2016-08-14 20:39:40 +0900

    コールバック以外は動く

--- app/src/main/java/jp/conpon/twittanu/MainActivity.java
+++ app/src/main/java/jp/conpon/twittanu/MainActivity.java
@@ -1,80 +1,23 @@
 package jp.conpon.twittanu;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.view.Menu;
 
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.User;
 import twitter4j.auth.AccessToken;
 
 /**
  * Created by Shigure on 2016/08/12.
  */
 public class MainActivity extends Activity {
-    private static final String TOKEN = "token";
-    private static final String TOKEN_SECRET = "token_secret";
-    private static final String PREF_NAME = "twitter_access_token";
-
-    public static Twitter getTwitterInstance(Context context) {
-        String consumerKey = context.getString(R.string.api_key);
-        String consumerSecret = context.getString(R.string.api_secret);
-
-        TwitterFactory factory = new TwitterFactory();
-        Twitter twitter = factory.getInstance();
-        twitter.setOAuthConsumer(consumerKey, consumerSecret);
-
-        AccessToken accessToken = loadAccessToken(context);
-        if (accessToken != null) {
-            twitter.setOAuthAccessToken(accessToken);
-        }
-        return twitter;
-    }
-
-    /**
-     * アクセストークンをプリファレンスから読み込みます。
-     *
-     * @param context
-     * @return
-     */
-    public static AccessToken loadAccessToken(Context context) {
-        SharedPreferences preferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        String token = preferences.getString(context.getString(R.string.token), null);
-        String tokenSecret = preferences.getString(context.getString(R.string.token_secret), null);
-        if (token != null && tokenSecret != null) {
-            return new AccessToken(token, tokenSecret);
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * アクセストークンをプリファレンスに保存します。
-     *
-     * @param context
-     * @param accessToken
-     */
-    public static void storeAccessToken(Context context, AccessToken accessToken) {
-        SharedPreferences preferences = context.getSharedPreferences(PREF_NAME,
-                Context.MODE_PRIVATE);
-        Editor editor = preferences.edit();
-        editor.putString(TOKEN, accessToken.getToken());
-        editor.putString(TOKEN_SECRET, accessToken.getTokenSecret());
-        editor.apply();
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AccessToken accessToken = loadAccessToken(this);
-        if (accessToken != null) {
+        AccessToken accessToken = TwitterUtils.loadAccessToken(this);
+        if (accessToken == null) {
             Intent intent = new Intent(getApplicationContext(), TwitterOAuthActivity.class);
             startActivity(intent);
             finish();
