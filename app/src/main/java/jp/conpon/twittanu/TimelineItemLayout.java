6bbfc2da41feaf35d13430bdd2a50094d9e5cc4b
mirage-residence <l.i.mirage-residence@hotmail.co.jp>
2017-02-02 00:24:03 +0900

    タイムラインビューに情報追加

--- app/src/main/java/jp/conpon/twittanu/TimelineItemLayout.java
+++ app/src/main/java/jp/conpon/twittanu/TimelineItemLayout.java
@@ -4,16 +4,25 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.Nullable;
+import android.text.format.DateFormat;
 import android.util.AttributeSet;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.Date;
+
+import twitter4j.User;
 
 /**
  * Created by mirage-residence on 2017/01/25.
  */
 
 public class TimelineItemLayout extends RelativeLayout {
+	private TextView userName;
 	private TextView content;
+	private TextView createdTime;
+	private TextView retweetCount;
+	private TextView favCount;
 
 	public TimelineItemLayout(Context context) {
 		super(context);
@@ -36,12 +45,31 @@ public class TimelineItemLayout extends RelativeLayout {
 	protected void onFinishInflate() {
 		super.onFinishInflate();
 
+		userName = (TextView) findViewById(R.id.user_name);
 		content = (TextView) findViewById(R.id.content);
+		createdTime = (TextView) findViewById(R.id.created_time);
+		retweetCount = (TextView) findViewById(R.id.retweet_count);
+		favCount = (TextView) findViewById(R.id.fav_count);
 	}
 
 	public void bindView(@Nullable final twitter4j.Status status) {
 		if (status != null) {
+			User user = status.getUser();
+
+			StringBuilder builder = new StringBuilder();
+			builder.append(user.getName());
+			builder.append(" @");
+			builder.append(user.getScreenName());
+			userName.setText(builder);
+
 			content.setText(status.getText());
+			createdTime.setText(getCreatedTimeString(status.getCreatedAt()));
+			retweetCount.setText(String.valueOf(status.getRetweetCount()));
+			favCount.setText(String.valueOf(status.getFavoriteCount()));
 		}
+	}
+
+	private CharSequence getCreatedTimeString(final Date createdAt) {
+		return DateFormat.format("H:mm - yyyy年M月d日", createdAt);
 	}
 }
