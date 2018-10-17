383060feeeed26eb7371b7bbb12c144964af5d3b
mirage-residence <l.i.mirage-residence@hotmail.co.jp>
2018-10-17 23:55:57 +0900

    ユーザーアイコン表示

--- app/src/main/java/jp/conpon/twittanu/TimelineItemLayout.java
+++ app/src/main/java/jp/conpon/twittanu/TimelineItemLayout.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.support.annotation.Nullable;
 import android.text.format.DateFormat;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -17,61 +18,62 @@ import twitter4j.User;
  * Created by mirage-residence on 2017/01/25.
  */
 public class TimelineItemLayout extends RelativeLayout {
-	private UrlImageView userIcon;
-	private TextView userName;
-	private TextView content;
-	private TextView createdTime;
-	private TextView retweetCount;
-	private TextView favCount;
+    private UrlImageView userIcon;
+    private TextView userName;
+    private TextView content;
+    private TextView createdTime;
+    private TextView retweetCount;
+    private TextView favCount;
 
-	public TimelineItemLayout(Context context) {
-		super(context);
-	}
+    public TimelineItemLayout(Context context) {
+        super(context);
+    }
 
-	public TimelineItemLayout(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public TimelineItemLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public TimelineItemLayout(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-	}
+    public TimelineItemLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
-	@TargetApi(Build.VERSION_CODES.LOLLIPOP)
-	public TimelineItemLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-		super(context, attrs, defStyleAttr, defStyleRes);
-	}
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public TimelineItemLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
 
-	@Override
-	protected void onFinishInflate() {
-		super.onFinishInflate();
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
 
-		userIcon = (UrlImageView) findViewById(R.id.user_icon);
-		userName = (TextView) findViewById(R.id.user_name);
-		content = (TextView) findViewById(R.id.content);
-		createdTime = (TextView) findViewById(R.id.created_time);
-		retweetCount = (TextView) findViewById(R.id.retweet_count);
-		favCount = (TextView) findViewById(R.id.fav_count);
-	}
+        userIcon = (UrlImageView) findViewById(R.id.user_icon);
+        userName = (TextView) findViewById(R.id.user_name);
+        content = (TextView) findViewById(R.id.content);
+        createdTime = (TextView) findViewById(R.id.created_time);
+        retweetCount = (TextView) findViewById(R.id.retweet_count);
+        favCount = (TextView) findViewById(R.id.fav_count);
+    }
 
-	public void bindView(@Nullable final twitter4j.Status status) {
-		if (status != null) {
-			User user = status.getUser();
-			userIcon.setImage(user.getProfileImageURL());
+    public void bindView(@Nullable final twitter4j.Status status) {
+        if (status != null) {
+            User user = status.getUser();
+            DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
+            userIcon.setImage(user.getProfileImageURL(), metrics.density); // 画面密度にあわせて画像拡大
 
-			StringBuilder builder = new StringBuilder();
-			builder.append(user.getName());
-			builder.append(" @");
-			builder.append(user.getScreenName());
-			userName.setText(builder);
+            StringBuilder builder = new StringBuilder();
+            builder.append(user.getName());
+            builder.append(" @");
+            builder.append(user.getScreenName());
+            userName.setText(builder);
 
-			content.setText(status.getText());
-			createdTime.setText(getCreatedTimeString(status.getCreatedAt()));
-			retweetCount.setText(String.valueOf(status.getRetweetCount()));
-			favCount.setText(String.valueOf(status.getFavoriteCount()));
-		}
-	}
+            content.setText(status.getText());
+            createdTime.setText(getCreatedTimeString(status.getCreatedAt()));
+            retweetCount.setText(String.valueOf(status.getRetweetCount()));
+            favCount.setText(String.valueOf(status.getFavoriteCount()));
+        }
+    }
 
-	private CharSequence getCreatedTimeString(final Date createdAt) {
-		return DateFormat.format("H:mm - yyyy年M月d日", createdAt);
-	}
+    private CharSequence getCreatedTimeString(final Date createdAt) {
+        return DateFormat.format("H:mm - yyyy年M月d日", createdAt);
+    }
 }
